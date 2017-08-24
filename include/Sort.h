74ac48c76ce6995f3675887caac2dffa9061c917
Sean <a1668979@student.adelaide.edu.au>
2017-08-24 07:40:38 +0100

    [FEATURE] Added support for the generic insertion sort

--- include/Sort.h
+++ include/Sort.h
@@ -6,12 +6,21 @@
 #include <iostream>
 #include <fstream>
 
-class Sort {
-  public:
-    Sort();
-    ~Sort();
+namespace csfoundation {
+    namespace sort {
+        template <class T> void insertion(std::vector<T> arr) {
+            for (size_t i = 1; i < arr.size(); ++i) {
+                size_t j = i;
 
-    static void insertion(std::vector<int> arr);
-};
+                while (j > 0 && arr[j] < arr[j-1]) {
+                    T temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    --j;
+                }
+            }
+        }
+    }
+}
 
 #endif
--- src/Sort.cpp
+++ src/Sort.cpp
@@ -1,24 +1,2 @@
-
 #include "Sort.h"
-
-
-Sort::Sort() {}
-Sort::~Sort() {}
-
-void Sort::insertion(std::vector<int> arr) {
-  unsigned int j, temp;
-
-	for (unsigned int i = 0; i < arr.size(); i++) {
-		j = i;
-
-    // While the element is smaller than the element at a lower index, move the element down the line
-		while (j > 0 && arr[j] < arr[j-1]){
-      // Swap the elements
-      temp = arr[j];
-      arr[j] = arr[j-1];
-      arr[j-1] = temp;
-      j--;
-    }
-  }
-}
 
