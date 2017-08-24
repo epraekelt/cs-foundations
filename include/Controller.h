e67f9cd02fca9f56df35473ea7bc3ed9cd9b60d6
Sean <a1668979@student.adelaide.edu.au>
2017-08-24 08:06:01 +0100

    [FEATURE] Adding generic support to controller [FEATURE] Continued using namespaces for csfoundation [WARN] Code does now use C++11 features

--- include/Controller.h
+++ include/Controller.h
@@ -1,5 +1,5 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
+#ifndef __CSFOUNDATIONS_CONTROLLER_H
+#define __CSFOUNDATIONS_CONTROLLER_H
 
 #include <vector>
 #include <string>
@@ -8,18 +8,45 @@
 #include <cstdio>
 #include <ctime>
 #include <iomanip>
+#include <functional>
+namespace csfoundation {
+    class Controller {
+    public:
+        Controller(bool verbose);
+        ~Controller();
 
-class Controller {
-  public:
-    Controller(bool verbose);
-    ~Controller();
+        void printInfo(std::string filename);
+    
+        template <class T> void printSample(std::vector<T> arr) {
+            for (auto& val : arr) {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
+        }
+    
+        template <class T> double runAlgorithm(std::string const docfile, 
+            std::function<void(std::vector<T>)> func, 
+            std::vector<T> data) {
+                std::cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " << std::endl;
+            std::clock_t start;
+            double runtime;
+            this->printInfo(docfile);
+        
+            start = std::clock();
+            if (verbose) {
+                this->printSample(data);
+            }
+            func(data);
+            runtime = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+            std::cout << std::fixed;
+            std::cout << "Sample size (" << data.size() << "): "  << std::setprecision(8)<< runtime 
+                << "s" << std::endl;
+            return runtime;
+        }
 
-    void printInfo(std::string filename);
-    void printSample(std::vector<int> arr);
-    void runAlgorithm(std::string docfile, void (*run)(std::vector<int>), std::vector<int> smallSample, std::vector<int> mediumSample, std::vector<int> largeSample);
+    private:
+        bool verbose;
 
-  private:
-    bool verbose;
-};
-
+    };
+}
 #endif
