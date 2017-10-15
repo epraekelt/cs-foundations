ff12af650eb4742931d60ec9968e6c56b20926f9
epraekelt <epraekelt@users.noreply.github.com>
2017-10-15 16:27:01 +1030

    [STASH] Some demo stuff, working highlightjs stuff

---
+++ public/javascripts/sorting/insertion.js
@@ -0,0 +1,20 @@
+
+let insertion = (array) => {
+  console.log('insertion sort');
+
+  var length = array.length;
+
+  for(var i = 1; i < length; i++) {
+    var temp = array[i];
+    for(var j = i - 1; j >= 0 && array[j] > temp; j--) {
+      array[j+1] = array[j];
+    }
+    array[j+1] = temp;
+  }
+
+  return array;
+}
+
+function runAlgo() {
+  wrapAlgo(insertion);
+}
