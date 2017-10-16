cf490b4ed479b6adcac1a4d353d28263844338f4
Jun Sun <qingyun95@gmail.com>
2017-10-16 20:35:31 +1030

    pages first commit

---
+++ javascripts/sorting/insertion.js
@@ -0,0 +1,18 @@
+
+let insertion = (array) => {
+  var length = array.length;
+
+  for(var i = 1; i < length; i++) {
+    var temp = array[i];
+    for(var j = i - 1; j >= 0 && array[j] > temp; j--) {
+      array[j+1] = array[j];
+    }
+    array[j+1] = temp;
+  }
+
+  return array;
+}
+
+function runAlgo() {
+  wrapAlgo(insertion);
+}
